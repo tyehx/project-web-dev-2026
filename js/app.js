acef02658ddb3d24372a43b6348600205edd28a0
tyehx <tugra.uysal@fluxus.nu>
2026-06-08 14:59:55 +0200

    CSS en JS 3 buttons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.querySelectorAll("button[data-target]");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var target = button.getAttribute("data-target");
+      if (target) {
+        window.location.href = target;
+      }
+    });
+  });
+});
 
