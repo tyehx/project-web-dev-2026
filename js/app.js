8521791b3bdc9be8bd866c251a40d814af3c4d92
tyehx <tugra.uysal@fluxus.nu>
2026-06-11 22:28:35 +0200

    javascript revamped and help knop bij game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+
   var buttons = document.querySelectorAll("button[data-target]");
   buttons.forEach(function (button) {
     button.addEventListener("click", function () {
@@ -8,47 +8,48 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
+
+  // shake
+  if (document.getElementById("shakeKnop")) {
+    document.getElementById("shakeKnop").addEventListener("click", function() {
+      document.body.classList.add("shake");
+      setTimeout(function() {
+        document.body.classList.remove("shake");
+      }, 500);
+    });
+  }
+
+  // game 
+  if (document.getElementById("regels")) {
+    document.getElementById("regels").addEventListener("change", controleerKnop);
+    document.getElementById("naamInvoer").addEventListener("input", controleerKnop);
+
+    document.getElementById("RollKnop").addEventListener("click", function() {
+      var cijfer1 = Math.floor(Math.random() * 10);
+      var cijfer2 = Math.floor(Math.random() * 10);
+      var cijfer3 = Math.floor(Math.random() * 10);
+
+      document.getElementById("resultaat").innerText = cijfer1 + " " + cijfer2 + " " + cijfer3;
+
+      if (cijfer1 === cijfer2 && cijfer2 === cijfer3) {
+        document.getElementById("bericht").innerText = "WIN!";
+      } else {
+        document.getElementById("bericht").innerText = "Probeer opnieuw!";
+      }
+    });
+  }
+
 
 function controleerKnop() {
-    var naam = document.getElementById("naamInvoer").value;
-    var checkbox = document.getElementById("regels").checked;
+  var naam = document.getElementById("naamInvoer").value;
+  var checkbox = document.getElementById("regels").checked;
 
-    if (naam !== "" && checkbox) {
-        document.getElementById("RollKnop").disabled = false; //knop word klikbaar
-    } else {
-        document.getElementById("RollKnop").disabled = true;  //knop word gedisabled
-    }
+  if (naam !== "" && checkbox) {
+    document.getElementById("RollKnop").disabled = false;
+  } else {
+    document.getElementById("RollKnop").disabled = true;
+  }
 }
-
-document.getElementById("regels").addEventListener("change", controleerKnop);
-document.getElementById("naamInvoer").addEventListener("input", controleerKnop);
-
-document.getElementById("RollKnop").addEventListener("click", function() {
-    // genereert random cijfer van 0-10
-    var cijfer1 = Math.floor(Math.random() * 10);
-    var cijfer2 = Math.floor(Math.random() * 10);
-    var cijfer3 = Math.floor(Math.random() * 10);
-
-    // gerollde cijfers tonen
-    document.getElementById("resultaat").innerText = cijfer1 + " " + cijfer2 + " " + cijfer3;
-
-    // zien of ze allemaal dezelfde zijn en als het zo is dan win als het niet zo is probeer opnieuw
-    if (cijfer1 === cijfer2 && cijfer2 === cijfer3) {
-        document.getElementById("bericht").innerText = "WIN!";
-    } else {
-        document.getElementById("bericht").innerText = "Probeer opnieuw!";
-    }
-});
-
-document.getElementById("shakeKnop").addEventListener("click", function() {
-  document.body.classList.add("shake");
-  setTimeout(function() {
-    document.body.classList.remove("shake");
-  }, 500);
-});
-
-
 
 //sources
 //https://www.w3schools.com/jsref/prop_checkbox_checked.asp 
