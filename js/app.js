5a866369b4027bbb33043bfa7961ea61b2d9ce5f
tyehx <tugra.uysal@fluxus.nu>
2026-06-11 17:22:39 +0200

    je kan nu rollen

--- js/app.js
+++ js/app.js
@@ -19,7 +19,26 @@ document.getElementById("regels").addEventListener("change", function() {
     }
 });
 
+document.getElementById("RollKnop").addEventListener("click", function() {
+    // genereert random cijfer van 0-10
+    var cijfer1 = Math.floor(Math.random() * 10);
+    var cijfer2 = Math.floor(Math.random() * 10);
+    var cijfer3 = Math.floor(Math.random() * 10);
 
-//sources voor checkbox \
+    // gerollde cijfers tonen
+    document.getElementById("resultaat").innerText = cijfer1 + " " + cijfer2 + " " + cijfer3;
+
+    // zien of ze allemaal dezelfde zijn en als het zo is dan win als het niet zo is probeer opnieuw
+    if (cijfer1 === cijfer2 && cijfer2 === cijfer3) {
+        document.getElementById("bericht").innerText = "WIN!";
+    } else {
+        document.getElementById("bericht").innerText = "Probeer opnieuw!";
+    }
+});
+
+//sources
 //https://www.w3schools.com/jsref/prop_checkbox_checked.asp 
 //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+//https://www.w3schools.com/js/js_random.asp
+//https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+//
